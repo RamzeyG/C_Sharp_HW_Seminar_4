897d518729410b43006d87d621c0bd23b5d8b73f
Ramzey <remzesii@gmail.com>
2022-05-28 02:35:27 +0500

    первое задание

---
+++ ex001/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+void Func(int A, int B){
+    int C = A;
+    if(A == 0 && B == 0){
+        Console.WriteLine($"Число {C} в степени {B} - выражение не имеет смысла");
+    }
+    else if(B == 0){
+        Console.WriteLine($"Число {C} в степени {B} - равно 1");
+    }
+    else if(B != 0){
+        int count = 1;
+        while(count < B){
+        A = A * C;
+        count++;
+        }
+    Console.WriteLine($"Число {C} в степени {B} равно {A}");
+    }    
+    
+}
+
+Func(4, 2);
+Func(3, 6);
+Func(12, 2);
+Func(11, 0);
+Func(0, 0);
+Func(0, 2);
