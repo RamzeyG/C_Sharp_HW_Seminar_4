c767d4bdcb87118c224d4dc32c6de737f5531f81
Ramzey <remzesii@gmail.com>
2022-05-28 04:01:02 +0500

    Сделано 3 задание

---
+++ ex003/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран. Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+
+void Func(){
+    
+    
+    int[] array = new int[8];
+    int count = 0;
+    int size = 8;
+
+    while (count < size){
+        int random = new Random().Next(-100,100);
+        
+        array[count] = random;
+        count++;
+    }
+    Console.WriteLine();
+    Console.WriteLine("Был созан массив: ");
+    foreach (int i in array){
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+    
+}
+
+Func();
+Func();
+Func();
+Func();
