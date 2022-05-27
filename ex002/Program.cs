7bcd0b756913d3b41978243dd250e65294992e27
Ramzey <remzesii@gmail.com>
2022-05-28 03:26:34 +0500

    Сделано второе задание

---
+++ ex002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+
+void Func(){
+    int a = new Random().Next(0, 999999);
+    string text = Convert.ToString(a);
+    
+    int count = 0;
+    int x = 0;
+    while(count < text.Length){
+        x = x + Convert.ToInt32(Convert.ToString(text[count]));
+        count++;
+    }
+    Console.WriteLine($"Сумма цифр числа {a} равна {x}");
+}
+
+Func();
+Func();
+Func();
+Func();
+
